e379f736695b03196067649472b25b6fb22369f3
Alex_B012 <rad15on2014@gmail.com>
2025-07-26 16:44:43 +0300

    third commit

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import "./header.css"
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { headerLinks, headerOuterLinks } from '../../data/data'
+import { headerLinks, headerOuterLinks, burgerBtns } from '../../data/data'
+
+
+
 
 function Header() {
+   const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+   // Toggle the menu state
+   const toggleMenu = () => {
+      setIsMenuOpen(!isMenuOpen);
+   };
+
+
    return (
       <header>
          <Link to={headerLinks[0].url} className='header__logoLink'><img src="/techHive4.svg" alt="Logo" className='header__logo' /></Link>
@@ -21,7 +32,7 @@ function Header() {
          </div>
 
          <div className='header__burger'>
-            <button className='header__burger-btn'>btn</button>
+            {isMenuOpen ? (<button className='header__burger-btn'><FontAwesomeIcon icon={burgerBtns[0].icon} className="header__closeBtn" /></button>) : (<button className='header__burger-btn'><FontAwesomeIcon icon={burgerBtns[1].icon} className="header__closeBtn" /></button>)}
          </div>
 
          <div className='header__cover'></div>
--- src/data/data.ts
+++ src/data/data.ts
@@ -2,6 +2,14 @@ import { faTelegram } from '@fortawesome/free-brands-svg-icons';
 import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 import { faYoutube } from '@fortawesome/free-brands-svg-icons';
 
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+
+export const burgerBtns = [
+  { id: 1, name: "Hamburger", icon: faBars  },
+  { id: 2, name: "Close", icon: faXmark  },
+];
+
 export const headerLinks = [
   { id: 1, name: "Home", url: "/" },
   { id: 2, name: "Pricing", url: "/pricing" },
@@ -13,3 +21,4 @@ export const headerOuterLinks = [
   { id: 2, name: "WhatsApp", url: "/pricing", icon: faWhatsapp},
   { id: 3, name: "YouTube", url: "/contacts", icon: faYoutube},
 ];
+
