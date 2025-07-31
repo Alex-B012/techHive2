924aa2c894393838069efc237431ebed47cc61e5
Alex_B012 <rad15on2014@gmail.com>
2025-07-31 14:44:59 +0300

    Pricing Page - completed

--- src/pages/Pricing/Pricing.tsx
+++ src/pages/Pricing/Pricing.tsx
@@ -9,8 +9,10 @@ function Pricing() {
          <div className="pricingPage__empty"></div>
          <div className="pricingPage__container">
             <PromoHotOffers arr_data={productCategories_data} title={"Our hottest offers"} />
+
             <PromoCatOffers arr_data={productCategories_data} title={"Back to School Picks"} />
             <PromoCatOffers arr_data={productCategories_data} title={"Shop by Category"} />
+
          </div>
       </div>
    )
