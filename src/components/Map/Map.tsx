9ac35e9b2a4ad05bedb2bdd3a10feafb2d205d69
Alex_B012 <rad15on2014@gmail.com>
2025-08-10 14:38:41 +0300

    final v1.0.1

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -4,6 +4,7 @@ function Map() {
    return (
       <div className="map__container">
          <iframe className="map__iframe"
+            title="TechHive Map"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d470.5167705776718!2d-122.24417510868312!3d37.50209391411741!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fa2159f5f4cd1%3A0x398b3a1d4d9638f2!2sBest%20Buy!5e0!3m2!1sru!2sru!4v1754000118134!5m2!1sru!2sru"
             width="600"
             height="450"
